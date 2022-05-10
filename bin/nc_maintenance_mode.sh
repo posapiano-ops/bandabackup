e0127a029ecafa83b477c1f9989356480f8b5e1f
posapiano-ops <rfiorito@outlook.it>
2022-05-10 12:03:13 +0200

    fix maintenarce mode script

--- bin/nc_maintenance_mode.sh
+++ bin/nc_maintenance_mode.sh
@@ -2,8 +2,10 @@
 # Based on RdH7
 # Run as "./nc_maintenance_mode.sh on" or "./nc_maintenance_mode.sh off" 
 
-if [ $1 = 'on'] then
+if [ $1 = "on" ]; then
     sudo -u apache php /var/www/html/nextcloud/occ maintenance:mode --on
-else
-    sudo -u apache php /var/www/html/nextcloud/occ maintenance:mode --off 
+elif [ $1 = "off" ]; then
+    sudo -u apache php /var/www/html/nextcloud/occ maintenance:mode --off
+else 
+    echo "Command not found"
 fi
